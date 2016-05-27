0e392f9c18543305b1af0044c9ae721d78e432b8
Alex Sansom <alex_sansom@hotmail.com>
2016-05-27 10:05:23 +0000

    Tweak shell script documentation

--- visit_pages.sh
+++ visit_pages.sh
@@ -19,8 +19,8 @@ echo "Started at $today $start_time"
 
 # If input file doesn't exist, exit with error message
 if [ ! -f $input_file ]; then
-    echo "Error: Provided input file does not exist"
-    exit 1
+   echo "Error: Provided input file does not exist"
+   exit 1
 fi
 
 # If output dir doesn't yet exist, create it
@@ -30,12 +30,13 @@ fi
 
 # Now we've got input file with URLs for each page, and the output dir exists...
 # Use wget to:
+#
 # Request for each line in the input file
 # Log each request to a log file
 # Retry requests up to 5 times
 # Delay for 1 second between each request
 # Store the each request response in the $page_output_dir directory
-# The --no-proxy ignores any environment proxies set up.
+# --no-proxy ignores any environment proxies set up
 wget --no-proxy -a $log_file -i $input_file -t 5 -w 1 -P $page_output_dir
 
 # Show we're done
